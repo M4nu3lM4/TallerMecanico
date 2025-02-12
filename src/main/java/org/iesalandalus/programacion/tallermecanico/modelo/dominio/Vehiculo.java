ddc85cfc02541e9d93996aa41c77bdc7617ff0cb
Manuel_mma <manumaral04@gmail.com>
2025-02-12 18:39:54 +0100

    Continuo empezando la clase vehiculo

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Vehiculo.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Vehiculo.java
@@ -1,7 +1,9 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.dominio;
 
+import java.util.Objects;
+
 public record Vehiculo(String marca, String modelo, String matricula) {
-    private static String ER_MARCA = "^[A-Z][a-z]+( [A-Z][a-z]+)$";
+    private static String ER_MARCA = "^(Seat|Land Rover|KIA|Rolls-Royce|SsangYong)$";
     private static String ER_MATRICULA = "^[0-9]{4}[A-Z]{3}$";
 
 
@@ -12,25 +14,35 @@ public record Vehiculo(String marca, String modelo, String matricula) {
     }
 
     private void validarMarca(String marca) {
+        Objects.requireNonNull(marca,"La marca no puede ser nula.");
+
         if (!marca.matches(ER_MARCA)) {
-            throw new IllegalArgumentException("Marca incorrecta.");
+            throw new IllegalArgumentException("La marca no tiene un formato válido.");
         }
+
     }
 
     private void validarMatricula(String matricula) {
+        Objects.requireNonNull(matricula,"La matricula no puede ser nula.");
+
+
         if (!matricula.matches(ER_MATRICULA)) {
-            throw new IllegalArgumentException("Matricula incorrecta.");
+            throw new IllegalArgumentException("La matrícula no tiene un formato válido.");
         }
     }
 
     private void validarModelo(String modelo) {
+        Objects.requireNonNull(modelo,"El modelo no puede ser nulo.");
+
         if (!modelo.matches(ER_MATRICULA)) {
-            throw new IllegalArgumentException("Modelo incorrecto.");
+            throw new IllegalArgumentException("El modelo no tiene un formato válido.");
         }
+
     }
 
-    public Vehiculo get(String matricula) {
-        return new Vehiculo(this.marca,this.matricula,this.modelo);
+    public static Vehiculo get(String matricula) {
+
+        return new Vehiculo("Desconocido","Desconocido", matricula);
     }
 
     @Override
@@ -42,10 +54,14 @@ public record Vehiculo(String marca, String modelo, String matricula) {
         return this.matricula.equals(vehiculo.matricula);
     }
 
+    @Override
+    public final int hashCode() {
+        return matricula.hashCode();
+    }
 
 
-
-
-
-
+    @Override
+    public String toString() {
+        return "Marca: " + marca + ", Modelo: " + modelo + ", Matricula: " + matricula;
+    }
 }
