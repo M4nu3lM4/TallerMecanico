1ac9a21754992d511cfb88ccad5c1a9455641b8c
Manuel_mma <manumaral04@gmail.com>
2025-02-24 10:13:43 +0100

    Clase Consola Comenzada

--- src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Consola.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Consola.java
@@ -1,17 +1,76 @@
 package org.iesalandalus.programacion.tallermecanico.vista;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Revision;
+import org.iesalandalus.programacion.tallermecanico.vista.Opcion;
+
+import java.time.LocalDate;
 
 public class Consola {
     private static final String CADENA_FORMATO_FECHA = "dd/MM/yyyy";
 
     private Consola(){}
 
-    public static void mostrarCabecera() {
-        System.out.println("====================================");
-        System.out.println("  Taller Mecánico - Gestión de Vehículos");
-        System.out.println("====================================");
-        System.out.println();
+    public static void mostrarCabecera(String mensaje) {
+        mensaje = "====Taller Mecanico====";
+        System.out.println(mensaje);
+        System.out.println("______________________");
+
     }
     public static void mostrarMenu(){
         Consola.mostrarCabecera();
     }
+    public static Opcion elegirOpcion(){
+
+    }
+    private static int LeerEntero(String mensaje){
+
+    }
+    private static float LeerReal(String mensaje){
+
+    }
+    private static String LeerCadena(String mensaje){
+
+    }
+    private static LocalDate LeerFecha(String mensaje){
+
+    }
+    public static Cliente leerCliente(){
+
+    }
+
+    public static Cliente leerClienteDni(){
+
+    }
+
+    public static String leerNuevoNombre(){
+
+    }
+
+    public static String leerNuevoTelefono(){
+
+    }
+
+    public static Vehiculo leerVehiculo(){
+
+    }
+
+    public static Vehiculo leerVehiculoMatricula(){
+
+    }
+
+    public static Revision leerRevision(){
+
+    }
+
+    public static int leerHoras(){
+
+    }
+    public static float leerPrecioMaterial(){
+
+    }
+    public static LocalDate leerFechaCierre(){
+
+    }
+
 }
