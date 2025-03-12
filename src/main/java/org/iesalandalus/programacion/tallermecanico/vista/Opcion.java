a87262cc7b53f1ea793912978f5861eadb5f217b
Manuel_mma <manumaral04@gmail.com>
2025-03-12 10:49:25 +0100

    Clase opcion corregida y mejorada

--- src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Opcion.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Opcion.java
@@ -1,32 +1,32 @@
 package org.iesalandalus.programacion.tallermecanico.vista;
 
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.HashMap;
 
 public enum Opcion {
-    INSERTAR_CLIENTE(0, "Insertar Cliente"),
-    BUSCAR_CLIENTE(1, "Buscar Cliente"),
-    BORRAR_CLIENTE(2, "Borrar Cliente"),
-    LISTAR_CLIENTES(3, "Listar Clientes"),
-    MODIFICAR_CLIENTE(4, "Modificar Cliente"),
-    INSERTAR_VEHICULO(5, "Insertar Vehículo"),
-    BUSCAR_VEHICULO(6, "Buscar Vehículo"),
-    BORRAR_VEHICULO(7, "Borrar Vehículo"),
-    LISTAR_VEHICULOS(8, "Listar Vehículos"),
-    INSERTAR_REVISION(9, "Insertar Revisión"),
-    BUSCAR_REVISION(10, "Buscar Revisión"),
-    BORRAR_REVISION(11, "Borrar Revisión"),
-    LISTAR_REVISIONES(12, "Listar Revisiones"),
-    LISTAR_REVISIONES_CLIENTE(13, "Listar Revisiones por Cliente"),
-    LISTAR_REVISIONES_VEHICULO(14, "Listar Revisiones por Vehículo"),
-    ANADIR_HORAS_REVISION(15, "Añadir Horas a Revisión"),
-    ANADIR_PRECIO_MATERIAL_REVISION(16, "Añadir Precio de Material a Revisión"),
-    CERRAR_REVISION(17, "Cerrar Revisión"),
-    SALIR(18, "Salir");
+    INSERTAR_CLIENTE(11, "Insertar Cliente"),
+    BUSCAR_CLIENTE(12, "Buscar Cliente"),
+    BORRAR_CLIENTE(13, "Borrar Cliente"),
+    LISTAR_CLIENTES(14, "Listar Clientes"),
+    MODIFICAR_CLIENTE(15, "Modificar Cliente"),
+    INSERTAR_VEHICULO(21, "Insertar Vehículo"),
+    BUSCAR_VEHICULO(22, "Buscar Vehículo"),
+    BORRAR_VEHICULO(23, "Borrar Vehículo"),
+    LISTAR_VEHICULOS(24, "Listar Vehículos"),
+    INSERTAR_REVISION(31, "Insertar Revisión"),
+    BUSCAR_REVISION(32, "Buscar Revisión"),
+    BORRAR_REVISION(33, "Borrar Revisión"),
+    LISTAR_REVISIONES(34, "Listar Revisiones"),
+    LISTAR_REVISIONES_CLIENTE(35, "Listar Revisiones por Cliente"),
+    LISTAR_REVISIONES_VEHICULO(36, "Listar Revisiones por Vehículo"),
+    ANADIR_HORAS_REVISION(37, "Añadir Horas a Revisión"),
+    ANADIR_PRECIO_MATERIAL_REVISION(38, "Añadir Precio de Material a Revisión"),
+    CERRAR_REVISION(39, "Cerrar Revisión"),
+    SALIR(0, "Salir");
 
     private final int numeroOpcion;
     private final String mensaje;
-    private static final Map<Integer, Opcion> opciones = new TreeMap<>();
+    private static final Map<Integer, Opcion> opciones = new HashMap<>();
 
     static {
         for (Opcion opcion : values()) {
@@ -52,6 +52,6 @@ public enum Opcion {
 
     @Override
     public String toString() {
-        return numeroOpcion + ". " + mensaje;
+        return String.format("%d.- %s%n ",numeroOpcion,mensaje);
     }
 }
