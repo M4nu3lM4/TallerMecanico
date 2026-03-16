caefb8a2b5261004d5750d2f5e61deed1edf26ff
Manuel_mma <manumaral04@gmail.com>
2026-03-16 19:57:42 +0100

    Cliente nombre regex corregida

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Cliente.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Cliente.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 public class Cliente {
 
-    private static final String ER_NOMBRE = "[A-Z][a-záéíóú]+( [A-Z][a-záéíóú]+)*";
+    private static final String ER_NOMBRE = "[A-ZÁÉÍÓÚÑ][a-záéíóúüñ]+(?: [A-ZÁÉÍÓÚ][a-záéíóúüñ]+)*+";
     private static final String ER_DNI = "\\d{8}[A-Z]";
     private static final String ER_TELEFONO= "\\d{9}";
 
