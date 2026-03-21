cab266ba2c973cef5ac77a9e6843f21c31f21429
Manuel_mma <manumaral04@gmail.com>
2026-03-21 02:53:46 +0100

    Enumerado Opcion terminado

--- src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Opcion.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Opcion.java
@@ -2,57 +2,58 @@ package org.iesalandalus.programacion.tallermecanico.vista;
 
 import org.iesalandalus.programacion.tallermecanico.modelo.TallerMecanicoExcepcion;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
+
 
 public enum Opcion {
-    INSERTAR_CLIENTE(1,"Insertar cliente."),
-    BUSCAR_CLIENTE(2,"Buscar cliente."),
-    BORRAR_CLIENTE(3,"Borrar cliente."),
-    LISTAR_CLIENTE(4,"Listar clientes."),
-    MODIFICAR_CLIENTE(5,"Modificar cliente."),
-    INSERTAR_VEHICULO(6,"Insertar vehículo."),
-    BUSCAR_VEHICULO(7,"Buscar vehículo."),
-    BORRAR_VEHICULO(8,"Borrar vehículo."),
-    LISTAR_VEHICULO(9,"Listar vehículos."),
-    INSERTAR_REVISION(10,"Insertar revisión."),
-    BUSCAR_REVISION(11,"Buscar revisión."),
-    BORRAR_REVISION(12,"Borrar revision."),
-    LISTAR_REVISIONES(13,"Listar revisiones."),
-    LISTAR_REVISIONES_CLIENTES(14,"Listar revisiones por cliente."),
-    LISTAR_REVISIONES_VEHICULOS(15,"Listar revisiones por vehiculo."),
-    ANADIR_HORAS_REVISION(16,"Añadir horas a la revisión."),
-    ANADIR_PRECIO_MATERIAL_REVISION(17,"añadir precio material a la revisión."),
-    CERRAR_REVISION(18,"Cerrar revisión."),
-    SALIR(19,"Salir.");
+    INSERTAR_CLIENTE(11, "Insertar cliente"),
+    BUSCAR_CLIENTE(12, "Buscar cliente"),
+    BORRAR_CLIENTE(13, "Borrar cliente"),
+    LISTAR_CLIENTES(14, "Listar clientes"),
+    MODIFICAR_CLIENTE(15, "Modificar cliente"),
+    INSERTAR_VEHICULO(21, "Insertar vehículo"),
+    BUSCAR_VEHICULO(22, "Buscar vehículo"),
+    BORRAR_VEHICULO(23, "Borrar vehículo"),
+    LISTAR_VEHICULOS(24, "Listar vehículos"),
+    INSERTAR_REVISION(31, "Insertar revisión"),
+    BUSCAR_REVISION(32, "Buscar revisión"),
+    BORRAR_REVISION(33,"Borrar revisión"),
+    LISTAR_REVISIONES(34, "Listar revisiones"),
+    LISTAR_REVISIONES_CLIENTE(35, "Listar revisiones de un cliente"),
+    LISTAR_REVISIONES_VEHICULO(36, "Listar revisiones de un vehículo"),
+    ANADIR_HORAS_REVISION(37, "Añadir horas a revisión"),
+    ANADIR_PRECIO_MATERIAL_REVISION(38, "Añadir precio de material a revisión"),
+    CERRAR_REVISION(39,"Cerrar revisión"),
+    SALIR(0, "Salir");
 
-    private int numeroOpcion;
-
-    private String mensaje;
-
-    private static Map<Integer,Opcion> opciones = new TreeMap<>();
-
-    private Opcion(int numeroOpcion,String mensaje){
-
+    private final int numeroOpcion;
+    private final String mensaje;
+    private static final Map<Integer, Opcion> opciones = new HashMap<>();
+    static {
+        for (Opcion op : Opcion.values()) {
+            opciones.put(op.numeroOpcion, op);
+        }
     }
 
-    public static boolean esValida(int numeroOpcion){
-        if (!opciones.containsKey(numeroOpcion)){
-            return false;
-        }
-        return true;
+    Opcion(int numeroOpcion, String mensaje) {
+        this.numeroOpcion = numeroOpcion;
+        this.mensaje = mensaje;
     }
 
-    public static Opcion get(int numeroOpcion) throws TallerMecanicoExcepcion {
+    public static boolean esValida(int numeroOpcion) {
+        return opciones.containsKey(numeroOpcion);
+    }
 
-        if (!esValida(numeroOpcion)){
-            throw new TallerMecanicoExcepcion("La opción no es válida");
+    public static Opcion get(int numeroOpcion) {
+        if(!esValida(numeroOpcion)) {
+            throw new IllegalArgumentException("Opción no válida.");
         }
-        return Opcion.get(numeroOpcion);
+        return opciones.get(numeroOpcion);
     }
 
     @Override
-    public String toString() {
-        return String.format("%s, %s", numeroOpcion, mensaje);
+    public String toString () {
+        return String.format("%s - %s", numeroOpcion, mensaje);
     }
 }
