69b8adabdce1f70902f753d08c671cf040857303
Manuel_mma <manumaral04@gmail.com>
2026-04-28 13:53:03 +0200

    negocio funciona

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/FabricaFuenteDatos.java
@@ -0,0 +1,16 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
+
+public enum FabricaFuenteDatos {
+
+    MEMORIA {
+        @Override
+        public IFuenteDatos crear() {
+            return null;
+        }
+    };
+
+
+
+    public abstract IFuenteDatos crear();
+
+}
